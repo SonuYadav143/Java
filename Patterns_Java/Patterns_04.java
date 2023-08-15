785f882b3dd0e8f7a6779e96b49c16229c265c73
Sonu_Yadav <134956732+SonuYadav143@users.noreply.github.com>
2023-08-15 15:21:20 +0000

    update

---
+++ Patterns_Java/Patterns_04.java
@@ -0,0 +1,15 @@
+package Patterns_Java;
+
+public class Patterns_04 {
+    public static void main(String[] args) {
+        int n=4;
+        //outer loop
+        for(int i=n; i>=1;i--){
+            //inner loop
+            for(int j=1; j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
