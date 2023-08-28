a1e447bdd83af0dd6e62b6d4b69891c0cc5f9a40
Sonu_Yadav <134956732+SonuYadav143@users.noreply.github.com>
2023-08-28 10:12:41 +0530

    Inharitance_opps_lecture

---
+++ Inharitance/Client.java
@@ -0,0 +1,9 @@
+package Inharitance;
+
+public class Client  extends Price{
+    void info(){
+        System.out.println("Suraj:--");
+
+    }
+
+}
---
+++ Inharitance1/sonu.java
@@ -0,0 +1,10 @@
+package Inharitance1;
+
+public class sonu extends sunil{
+    void info(){
+        System.out.println("My Name :-Sonu:--");
+    }
+
+    public void info1() {
+    }
+}
