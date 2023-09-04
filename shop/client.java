6ed4e54af0d1527895acbde1316229ba1f4d70f3
Sonu_Yadav <134956732+SonuYadav143@users.noreply.github.com>
2023-09-04 11:08:39 +0530

    Add files via upload

---
+++ shop/client.java
@@ -0,0 +1,10 @@
+package shop;
+
+public class client {
+    String name;
+    long number;
+     void info(){
+         System.out.println("Enter your name:-"+name);
+         System.out.println("Enter your contact number:-"+number);
+     }
+}
---
+++ shop/client_main.java
@@ -0,0 +1,7 @@
+package shop;
+import java.util.Scanner;
+public class client_main {
+    public static void main(String[] args) {
+
+    }
+}
